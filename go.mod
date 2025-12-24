a722d826a17b68b212acf18b2735f87ba9a971dc
Luca Fondo <luca.fondo@trai.ch>
2025-12-24 15:12:45 +0100

    feat(telemetry): scaffold progrock adapter

--- go.mod
+++ go.mod
@@ -16,8 +16,10 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
 	golang.org/x/mod v0.27.0 // indirect
 	golang.org/x/tools v0.36.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
---
+++ internal/adapters/telemetry/progrock/recorder.go
@@ -0,0 +1,20 @@
+package progrock
+
+import (
+	"context"
+
+	"go.trai.ch/bob/internal/core/ports"
+)
+
+// Recorder implements the ports.Telemetry interface using the apps/progrock library.
+type Recorder struct{}
+
+// Record starts recording a new vertex.
+func (r *Recorder) Record(ctx context.Context, name string, opts ...ports.VertexOption) (context.Context, ports.Vertex) {
+	panic("not implemented")
+}
+
+// Close flushes and closes the recording session.
+func (r *Recorder) Close() error {
+	panic("not implemented")
+}
